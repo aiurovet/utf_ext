736b9da3d95684c3164b6636192323e432390313
aiurovet <aiurovet@gmail.com>
2023-03-01 23:28:26 +1100

    WIP

--- test/utf_decoder_sink_test.dart
+++ test/utf_decoder_sink_test.dart
@@ -1,37 +1,12 @@
 import 'package:test/test.dart';
 import 'package:utf_ext/utf_ext.dart';
 
+import 'utf.dart';
+
 /// A suite of tests for UtfDecoderSink
 ///
 void main() {
-  final abcUtf8 = <int>[
-    0xEF,
-    0xBB,
-    0xBF,
-    0x41,
-    0x42,
-    0x43,
-  ];
-  final abcUtf32be = <int>[
-    0x00,
-    0x00,
-    0xFE,
-    0xFF,
-    0x00,
-    0x00,
-    0x00,
-    0x41,
-    0x00,
-    0x00,
-    0x00,
-    0x42,
-    0x00,
-    0x00,
-    0x00,
-    0x43,
-  ];
-
-  group('constructor -', () {
+  group('convert -', () {
     test('UTF-8 - onBom', () {
       var wasCalled = false;
       UtfDecoderSink(
@@ -46,20 +21,20 @@ void main() {
     });
     test('UTF-8 - just BOM', () {
       final decoder = UtfDecoderSink(id: null, type: UtfType.utf8);
-      expect(decoder.convert(abcUtf8, 0, 3), "");
+      expect(decoder.convert(Utf.bytesUtf8, 0, 3), "");
     });
     test('UTF-8 - change of BOM', () {
       final decoder = UtfDecoderSink(id: null, type: UtfType.utf8);
-      decoder.convert(abcUtf32be, 0, 3);
+      decoder.convert(Utf.bytesUtf32be, 0, 3);
       expect(decoder.type, UtfType.utf32be);
     });
     test('UTF-8 - convert start', () {
       final decoder = UtfDecoderSink(id: null, type: UtfType.utf8);
-      expect(decoder.convert(abcUtf8), "ABC");
+      expect(decoder.convert(Utf.bytesUtf8), Utf.complexStr);
     });
     test('UTF-8 - convert middle', () {
       final decoder = UtfDecoderSink(id: null, type: UtfType.utf8);
-      expect(decoder.convert(abcUtf8, 4), "BC");
+      expect(decoder.convert(Utf.bytesUtf8, 4), Utf.complexStr.substring(1));
     });
     test('UTF-32BE - onBom', () {
       var wasCalled = false;
@@ -75,20 +50,21 @@ void main() {
     });
     test('UTF-32BE - just BOM', () {
       final decoder = UtfDecoderSink(id: null, type: UtfType.utf32be);
-      expect(decoder.convert(abcUtf32be, 0, 4), "");
+      expect(decoder.convert(Utf.bytesUtf32be, 0, 4), "");
     });
     test('UTF-32BE - change of BOM', () {
       final decoder = UtfDecoderSink(id: null, type: UtfType.utf32be);
-      decoder.convert(abcUtf8, 0, 4);
+      decoder.convert(Utf.bytesUtf8, 0, 4);
       expect(decoder.type, UtfType.utf8);
     });
     test('UTF-32BE - convert start', () {
       final decoder = UtfDecoderSink(id: null, type: UtfType.utf32be);
-      expect(decoder.convert(abcUtf32be), "ABC");
+      expect(decoder.convert(Utf.bytesUtf32be, 0, 4 * (1 + 10)),
+          Utf.complexStr.substring(0, 10));
     });
     test('UTF-32BE - convert middle', () {
       final decoder = UtfDecoderSink(id: null, type: UtfType.utf32be);
-      expect(decoder.convert(abcUtf32be, 8), "BC");
+      expect(decoder.convert(Utf.bytesUtf32be, 8), Utf.complexStr.substring(1));
     });
   });
 }
